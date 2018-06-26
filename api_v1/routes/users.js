ae18ef8037b4a061e224b3ccde5f20d609f07356
Oleg Medviediev <oleg.medviediev@masterofcode.com>
2018-06-26 23:23:02 +0300

    rewrite to async/await

--- api_v1/routes/users.js
+++ api_v1/routes/users.js
@@ -12,75 +12,70 @@ const User = require('../models/user');
 // });
 
 /* GET users listing */
-router.get('/', function (req, res, next) {
-    User.find()
-        .exec()
-        .then(users => {
-            res.status(200).json(users)
-        })
-        .catch(err => {
-            const error = new Error(err);
-            error.status = 404;
-            next(error);
-        })
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await User.find().exec();
+        res.status(200).json(users)
+    } catch (err) {
+        const error = new Error(err);
+        error.status = 404;
+        next(error);
+    }
 });
 
 /* GET user by id */
-router.get('/:userId', function (req, res, next) {
+router.get('/:userId', async (req, res, next) => {
     const id = req.params.userId;
-    User.findById(id)
-        .exec()
-        .then(user => {
-            res.status(200).json(user)
-        })
-        .catch(err => {
-            const error = new Error(err);
-            error.status = 404;
-            next(error);
-        })
+    try {
+        const user = await User.findById(id).exec();
+        res.status(200).json(user)
+    } catch (err) {
+        const error = new Error(err);
+        error.status = 404;
+        next(error);
+    }
 });
 
 /* POST user */
 
-router.post('/create', function (req, res, next) {
-    User.find({email: req.body.email})
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: "Mail exists"
-                });
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        })
-                    } else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            username: req.body.username,
-                            password: hash,
-                            email: req.body.email
-                        });
-                        user.save()
-                            .then(result => {
-                                res.status(201).json({
-                                    username: result.username,
-                                    password: result.password,
-                                    email: result.email
-                                });
-                            })
-                            .catch(err => {
-                                const error = new Error(err);
-                                error.status = 404;
-                                next(error);
-                            });
-                    }
-                });
-            }
+router.post('/create', async (req, res, next) => {
 
+    const user = await User.find({email: req.body.email}).exec();
+
+    if (user.length >= 1) {
+        return res.status(409).json({
+            message: "Mail exists"
         });
+    } else {
+        bcrypt.hash(req.body.password, 10, async (err, hash) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err
+                })
+            } else {
+                const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    username: req.body.username,
+                    password: hash,
+                    email: req.body.email
+                });
+
+                try {
+                    const result = await user.save();
+                    res.status(201).json({
+                        username: result.username,
+                        password: result.password,
+                        email: result.email
+                    });
+                } catch (err) {
+                    const error = new Error(err);
+                    error.status = 404;
+                    next(error);
+                }
+            }
+        });
+    }
+
 });
 
 module.exports = router;
